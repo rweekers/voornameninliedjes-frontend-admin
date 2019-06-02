042156cc6692b5d28db9a01828696823346d46f0
Remco Weekers <remco@craftsmen.nl>
2019-06-02 22:58:17 +0200

    Some form styling (WiP))

--- src/SongDetail/SongDetail.js
+++ src/SongDetail/SongDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { songService } from '../services/song.service';
+import './SongDetail.css';
 
 class SongDetail extends React.Component {
     constructor(props) {
@@ -48,38 +49,49 @@ class SongDetail extends React.Component {
             <div>
                 <h2>{song.artist} - {song.title}</h2>
                 <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Artist: <input type="text" name="artist" value={this.state.song.artist} onChange={this.handleChange} />
-                    </label><br />
-                    <label>
-                        Name: <input type="text" name="name" value={this.state.song.name} onChange={this.handleChange} />
-                    </label><br />
-                    <label>
-                        Title: <input type="text" name="title" value={this.state.song.title} onChange={this.handleChange} />
-                    </label><br />
-                    <label>
-                        Background: <textarea name="background" value={this.state.song.background} onChange={this.handleChange} />
-                    </label><br />
-                    <label>
-                        YouTube: <textarea name="youtube" value={this.state.song.youtube} onChange={this.handleChange} />
-                    </label><br />
-                    <label>
-                        Spotify: <textarea name="spotify" value={this.state.song.spotify} onChange={this.handleChange} />
-                    </label><br />
-                    <label>
-                        Status: <select name="status" value={this.state.song.status} onChange={this.handleChange}>
-                            <option value="SHOW">Tonen</option>
-                            <option value="IN_PROGRESS">Aan het bewerken</option>
-                            <option selected value="TO_BE_DELETED">Verwijderd</option>
-                        </select>
-                    </label><br />
-                    {song.flickrPhotos.map((item, index) => (
-                        <label key={index}>
-                            Flickr photo {index + 1}: <input type="text" name={'flickrPhotos'} value={item} onChange={event => this.handleArrayChange(event, index)} />
-                        </label>
-                    ))}
-                    <br />
-                    <input type="submit" value="Submit" />
+                    <fieldset>
+                        <legend>Hi!</legend>
+                        <div>
+                            <label>Artist:</label>
+                            <input type="text" name="artist" value={this.state.song.artist} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Name:</label>
+                            <input type="text" name="name" value={this.state.song.name} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Title:</label>
+                            <input type="text" name="title" value={this.state.song.title} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Background:</label>
+                            <textarea name="background" value={this.state.song.background} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>YouTube:</label>
+                            <textarea name="youtube" value={this.state.song.youtube} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Spotify:</label>
+                            <textarea name="spotify" value={this.state.song.spotify} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Status:</label>
+                            <select name="status" value={this.state.song.status} onChange={this.handleChange}>
+                                <option value="SHOW">Tonen</option>
+                                <option value="IN_PROGRESS">Aan het bewerken</option>
+                                <option selected value="TO_BE_DELETED">Verwijderd</option>
+                            </select>
+                        </div>
+                        {song.flickrPhotos.map((item, index) => (
+                            <div>
+                                <label key={index}>Flickr photo {index + 1}:</label>
+                                <input type="text" name={'flickrPhotos'} value={item} onChange={event => this.handleArrayChange(event, index)} />
+                            </div>
+                        ))}
+                        <br />
+                        <input type="submit" value="Submit" />
+                    </fieldset>
                 </form>
             </div >
         );
