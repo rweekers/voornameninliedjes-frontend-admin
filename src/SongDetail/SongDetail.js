3738369293edfeebb95bd5506910ff8a980d12b8
Remco Weekers <rweekers@bol.com>
2019-06-05 16:43:20 +0200

    Add some setup for grid layout

--- src/SongDetail/SongDetail.js
+++ src/SongDetail/SongDetail.js
@@ -46,59 +46,63 @@ class SongDetail extends React.Component {
     render() {
         const song = this.state.song;
         return (
-            <div>
-                <h2>{song.artist} - {song.title}</h2>
+            <div className="Songdetail">
+                <header className="song-title"><h2>{song.artist} - {song.title}</h2></header>
                 <form onSubmit={this.handleSubmit}>
-                    <fieldset>
-                        <legend>Details</legend>
-                        <div class="line">
-                            <label>Artiest:</label>
-                            <input type="text" name="artist" value={this.state.song.artist} onChange={this.handleChange} />
-                        </div>
-                        <div class="line">
-                            <label>Naam:</label>
-                            <input type="text" name="name" value={this.state.song.name} onChange={this.handleChange} />
-                        </div>
-                        <div class="line">
-                            <label>Title:</label>
-                            <input type="text" name="title" value={this.state.song.title} onChange={this.handleChange} />
-                        </div>
-                        <div class="line">
-                            <label>Achtergrond:</label>
-                            <textarea name="background" value={this.state.song.background} onChange={this.handleChange} />
-                        </div>
-                        <div class="line">
-                            <label>YouTube:</label>
-                            <input type="text" name="youtube" value={this.state.song.youtube} onChange={this.handleChange} />
-                        </div>
-                        <div class="line">
-                            <label>Spotify:</label>
-                            <input type="text" name="spotify" value={this.state.song.spotify} onChange={this.handleChange} />
-                        </div>
-                        <div class="line">
-                            <label>Status:</label>
-                            <select name="status" value={this.state.song.status} onChange={this.handleChange}>
-                                <option value="SHOW">Tonen</option>
-                                <option value="IN_PROGRESS">Aan het bewerken</option>
-                                <option selected value="TO_BE_DELETED">Verwijderd</option>
-                            </select>
-                        </div>
-                        <div class="line">
-                            {song.flickrPhotos.map((item, index) => (
-                                <div>
-                                    <label key={index}>Flickr foto {index + 1}:</label>
-                                    <input type="text" name={'flickrPhotos'} value={item} onChange={event => this.handleArrayChange(event, index)} />
-                                </div>
-                            ))}
-                        </div>
-                    </fieldset>
-                    <fieldset>
-                        <legend>Achtergrond en media</legend>
-                        <div class="line">
-                            <label>Achtergrond:</label>
-                            <textarea name="background" value={this.state.song.background} onChange={this.handleChange} />
-                        </div>
-                    </fieldset>
+                    <content className="song-details">
+                        <fieldset>
+                            <legend>Details</legend>
+                            <div class="line">
+                                <label>Artiest:</label>
+                                <input type="text" name="artist" value={this.state.song.artist} onChange={this.handleChange} />
+                            </div>
+                            <div class="line">
+                                <label>Naam:</label>
+                                <input type="text" name="name" value={this.state.song.name} onChange={this.handleChange} />
+                            </div>
+                            <div class="line">
+                                <label>Title:</label>
+                                <input type="text" name="title" value={this.state.song.title} onChange={this.handleChange} />
+                            </div>
+                            <div class="line">
+                                <label>Achtergrond:</label>
+                                <textarea name="background" value={this.state.song.background} onChange={this.handleChange} />
+                            </div>
+                            <div class="line">
+                                <label>YouTube:</label>
+                                <input type="text" name="youtube" value={this.state.song.youtube} onChange={this.handleChange} />
+                            </div>
+                            <div class="line">
+                                <label>Spotify:</label>
+                                <input type="text" name="spotify" value={this.state.song.spotify} onChange={this.handleChange} />
+                            </div>
+                            <div class="line">
+                                <label>Status:</label>
+                                <select name="status" value={this.state.song.status} onChange={this.handleChange}>
+                                    <option value="SHOW">Tonen</option>
+                                    <option value="IN_PROGRESS">Aan het bewerken</option>
+                                    <option selected value="TO_BE_DELETED">Verwijderd</option>
+                                </select>
+                            </div>
+                            <div class="line">
+                                {song.flickrPhotos.map((item, index) => (
+                                    <div>
+                                        <label key={index}>Flickr foto {index + 1}:</label>
+                                        <input type="text" name={'flickrPhotos'} value={item} onChange={event => this.handleArrayChange(event, index)} />
+                                    </div>
+                                ))}
+                            </div>
+                        </fieldset>
+                    </content>
+                    <content className="song-metadata">
+                        <fieldset>
+                            <legend>Achtergrond en media</legend>
+                            <div class="line">
+                                <label>Achtergrond:</label>
+                                <textarea name="background" value={this.state.song.background} onChange={this.handleChange} />
+                            </div>
+                        </fieldset>
+                    </content>
                     <input type="submit" value="Opslaan" />
                 </form>
             </div >
