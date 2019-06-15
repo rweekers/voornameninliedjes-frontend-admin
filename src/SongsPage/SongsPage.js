8cdaea45cf573e52e30a1230711bb704051175ec
Remco Weekers <remco@craftsmen.nl>
2019-06-15 09:55:37 +0200

    Add toggle buttons song list

--- src/SongsPage/SongsPage.js
+++ src/SongsPage/SongsPage.js
@@ -7,12 +7,19 @@ class SongsPage extends React.Component {
         super(props);
 
         this.state = {
-            songs: []
+            songs: [],
+            filter: 'SHOW'
         };
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
         songService.getDone().then(songs => this.setState({ songs }));
+    }
+
+    handleClick(status) {
+        this.setState({ filter: status })        
     }
 
     render() {
@@ -20,9 +27,18 @@ class SongsPage extends React.Component {
         return (
             <div>
                 <h3>Songs</h3>
+                <button onClick={(e) => this.handleClick('SHOW')} disabled={this.state.filter === 'SHOW'}>
+                    Show active
+                </button>
+                <button onClick={(e) => this.handleClick('IN_PROGRESS')} disabled={this.state.filter === 'IN_PROGRESS'}>
+                    Show in progress
+                </button>
+                <button onClick={(e) => this.handleClick('TO_BE_DELETED')} disabled={this.state.filter === 'TO_BE_DELETED'}>
+                    Show to be deleted
+                </button>
                 {songs.length > 0 &&
                     <ul>
-                        {songs.filter(song => song.status === 'SHOW').map((song, index) =>
+                        {songs.filter(song => song.status === this.state.filter).map((song, index) =>
                             <li key={song.id}>
                                 <Link to={'/songs/' + song.id}>{song.artist} - {song.title} [{song.status}]</Link>
                             </li>
