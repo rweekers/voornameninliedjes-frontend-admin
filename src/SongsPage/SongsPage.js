9c592b979af563e5d6ec1dd9e29424b048c8e59c
Remco Weekers <remco@craftsmen.nl>
2019-09-23 21:30:43 +0200

    Add material ui components

--- src/SongsPage/SongsPage.js
+++ src/SongsPage/SongsPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { songService } from '../services/song.service';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class SongsPage extends React.Component {
     constructor(props) {
@@ -19,7 +21,7 @@ class SongsPage extends React.Component {
     }
 
     handleClick(status) {
-        this.setState({ filter: status })        
+        this.setState({ filter: status })
     }
 
     render() {
@@ -27,21 +29,27 @@ class SongsPage extends React.Component {
         return (
             <div>
                 <h3>Songs</h3>
-                <button onClick={(e) => this.handleClick('SHOW')} disabled={this.state.filter === 'SHOW'}>
+                <Button onClick={(e) => this.handleClick('SHOW')} disabled={this.state.filter === 'SHOW'} variant="contained" color="primary">
                     Show active
-                </button>
-                <button onClick={(e) => this.handleClick('IN_PROGRESS')} disabled={this.state.filter === 'IN_PROGRESS'}>
+                </Button>
+                <Button onClick={(e) => this.handleClick('IN_PROGRESS')} disabled={this.state.filter === 'IN_PROGRESS'} variant="contained" color="primary">
                     Show in progress
-                </button>
-                <button onClick={(e) => this.handleClick('TO_BE_DELETED')} disabled={this.state.filter === 'TO_BE_DELETED'}>
+                </Button>
+                <Button onClick={(e) => this.handleClick('TO_BE_DELETED')} disabled={this.state.filter === 'TO_BE_DELETED'} variant="contained" color="primary">
                     Show to be deleted
-                </button>
-                <Link to={'/songs/new'}>Nieuw nummer invoeren</Link>
+                </Button>
+                <Link to={'/songs/new'}>
+                    <Button variant="contained" color="secondary">
+                        Nieuw nummer invoeren
+                </Button>
+                </Link>
                 {songs.length > 0 &&
                     <ul>
                         {songs.filter(song => song.status === this.state.filter).map((song, index) =>
                             <li key={song.id}>
-                                <Link to={'/songs/' + song.id}>{song.artist} - {song.title} [{song.status}]</Link>
+                                <Typography variant="body1" gutterBottom>
+                                    <Link to={'/songs/' + song.id}>{song.artist} - {song.title} [{song.status}]</Link>
+                                </Typography>
                             </li>
                         )}
                     </ul>
