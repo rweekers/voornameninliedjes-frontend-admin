51701cd52a7e445333fbf43f396351d1af31e0d0
Remco Weekers <rweekers@bol.com>
2019-09-30 22:20:55 +0200

    Further setup filtering

--- src/SongsPage/SongsPage.js
+++ src/SongsPage/SongsPage.js
@@ -8,7 +8,10 @@ import Typography from '@material-ui/core/Typography';
 import './SongsPage.css';
 import MaterialButton from '../material-coponents/MaterialButton';
 import { withStyles } from '@material-ui/styles';
-import MaterialPaper from '../material-coponents/MaterialPaper';
+// import MaterialPaper from '../material-coponents/MaterialPaper';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 const styles = theme => ({
     root: {
@@ -29,19 +32,23 @@ class SongsPage extends React.Component {
 
         this.state = {
             songs: [],
-            filter: 'SHOW'
+            filters: ['SHOW']
         };
-
-        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
         songService.getDone().then(songs => this.setState({ songs }));
     }
 
-    handleClick(status) {
-        this.setState({ filter: status })
-    }
+    handleChange = name => event => {
+        const filterValue = event.target.value;
+        if (this.state.filters.includes(filterValue)) {
+            this.setState({ filters: this.state.filters.filter(i => i !== filterValue) });
+
+        } else {
+            this.setState({ filters: this.state.filters.concat(filterValue) });
+        }
+    };
 
     render() {
         const { songs } = this.state;
@@ -50,15 +57,25 @@ class SongsPage extends React.Component {
         return (
             <div>
                 <Typography variant="h3" gutterBottom>Nummers</Typography>
-                <Button onClick={(e) => this.handleClick('SHOW')} disabled={this.state.filter === 'SHOW'} variant="contained" color="primary">
-                    Actieve nummers
-                </Button>
-                <Button onClick={(e) => this.handleClick('IN_PROGRESS')} disabled={this.state.filter === 'IN_PROGRESS'} variant="contained" color="primary">
-                    Nummers in bewerking
-                </Button>
-                <Button onClick={(e) => this.handleClick('TO_BE_DELETED')} disabled={this.state.filter === 'TO_BE_DELETED'} variant="contained" color="primary">
-                    Nummers te verwijderen
-                </Button>
+                <FormGroup row>
+                    <FormControlLabel
+                        control={
+                            <Switch checked={this.state.filters.includes('SHOW')} onChange={this.handleChange('SHOW')} value="SHOW" />
+                        }
+                        label="Actieve nummers"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Switch checked={this.state.filters.includes('IN_PROGRESS')} onChange={this.handleChange('IN_PROGRESS')} value="IN_PROGRESS" />
+                        }
+                        label="Nummers in bewerking"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Switch checked={this.state.filters.includes('TO_BE_DELETED')} onChange={this.handleChange('TO_BE_DELETED')} value="TO_BE_DELETED" />
+                        }
+                        label="Nummers te verwijderen"
+                    /></FormGroup>
                 <Link to={'/songs/new'}>
                     <Button variant="contained" color="secondary">
                         Nieuw nummer invoeren
@@ -67,9 +84,9 @@ class SongsPage extends React.Component {
                 </Link>
                 {songs.length > 0 &&
                     <div>
-                        {songs.filter(song => song.status === this.state.filter).map((song, index) =>
+                        {songs.filter(song => this.state.filters.includes(song.status)).map((song, index) =>
                             <Link key={song.id} to={'/songs/' + song.id}>
-                                <MaterialPaper />
+                                {/* <MaterialPaper /> */}
                                 <Paper className={classes.root}>
                                     <Typography variant="h5" component="h3">
                                         {song.artist}
@@ -92,10 +109,3 @@ SongsPage.propTypes = {
 };
 
 export default withStyles(styles)(SongsPage);
-
-// SongsPage.propTypes = {
-//     classes: PropTypes.object.isRequired,
-//   };
-
-// export default withStyles(styles)(SongsPage);
-// export { SongsPage };
